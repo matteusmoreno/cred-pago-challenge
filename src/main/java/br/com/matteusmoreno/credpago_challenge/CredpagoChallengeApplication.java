53b0f0256134bf5c1e88b41b0028ed7f8994c4ee
Matteus Moreno <matteus_moreno@live.com>
2024-10-16 09:38:24 -0300

    initial commit

---
+++ src/main/java/br/com/matteusmoreno/credpago_challenge/CredpagoChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.matteusmoreno.credpago_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredpagoChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CredpagoChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/matteusmoreno/credpago_challenge/CredpagoChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matteusmoreno.credpago_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredpagoChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
